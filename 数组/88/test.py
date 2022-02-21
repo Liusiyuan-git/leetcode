cfe63ba07be59bc8f9b25384ccef5921f5fc2fe2
liusiyuan <945212191@qq.com>
2022-02-21 21:38:04 +0800

    leetcode 88 practice

---
+++ 数组/88/test.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        j = n - 1
+        for k in range(m + n - 1, -1, -1):
+            if j < 0 or i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+
+
+s = Solution()
+s.merge([2, 0], 1, [1], 1)
