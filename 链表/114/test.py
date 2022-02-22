81f928b6759d7603f0125a0a8a960de86dd9cb47
liusiyuan <945212191@qq.com>
2022-02-22 21:52:45 +0800

    leetcode 114 practice

---
+++ 链表/114/test.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head):
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            head = head.next
+            if head == fast:
+                return True
+        return False
