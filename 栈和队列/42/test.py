ba428088675ba343826d37f763e48992fb92f644
liusiyuan <945212191@qq.com>
2022-02-25 11:28:01 +0800

    leetcode 42 practice

--- 栈和队列/42/test.py
+++ 栈和队列/42/test.py
@@ -17,3 +17,24 @@ class Solution:
                 ans += accumulateWidth * (up - bottom)
             stack.append({"width": accumulateWidth + 1, "height": height})
         return ans
+
+#法二
+    def trap(self, heights) -> int:
+        ans = 0
+        n = len(heights)
+        preMax = [0] * n
+        surfMax = [0] * n
+        preMax[0] = heights[0]
+        surfMax[n - 1] = heights[n - 1]
+        for i in range(1, n):
+            preMax[i] = max(preMax[i - 1], heights[i])
+
+        for i in range(n - 2, -1, -1):
+            surfMax[i] = max(surfMax[i + 1], heights[i])
+
+        for i in range(1, n - 1):
+            up = min(preMax[i - 1], surfMax[i + 1])
+            bottom = heights[i]
+            if up > bottom:
+                ans += up - bottom
+        return ans
