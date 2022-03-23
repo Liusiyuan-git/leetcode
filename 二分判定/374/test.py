9f84db7263643c6ef6a1cbd64917c69e7d9531a7
liusiyuan <945212191@qq.com>
2022-03-23 09:52:13 +0800

    leetcode 374 practice

---
+++ 二分判定/374/test.py
@@ -0,0 +1,18 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+def guess(num: int) -> int:
+    pass
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left = 0
+        right = n
+        while left < right:
+            mid = (left + right) // 2
+            if guess(mid) <= 0:
+                right = mid
+            elif guess(mid) == 1:
+                left = mid + 1
+        return right
