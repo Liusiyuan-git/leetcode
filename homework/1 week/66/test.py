328d4a286928818976e3a78da809ae535f4f3d65
liusiyuan <945212191@qq.com>
2022-04-25 22:56:29 +0800

    practice 66

--- homework/1 week/66/test.py
+++ homework/1 week/66/test.py
@@ -1,15 +1,12 @@
 class Solution:
     def plusOne(self, digits):
-        result = []
-        add = 1
-        for i in range(len(digits) - 1, -1, -1):
-            count = digits[i] + add
-            if count >= 10:
-                add = 1
-                result = [count - 10] + result
-            else:
-                add = 0
-                result = [count] + result
-        if add:
-            result = [1] + result
-        return result
+        n = len(digits)
+        for i in range(n-1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                for j in range(i+1, n):
+                    digits[j] = 0
+                return digits
+        ans = [0] * (n + 1)
+        ans[0] = 1
+        return ans
