f8489d980a6d817c997df07c923cadd40dd773ec
liusiyuan <945212191@qq.com>
2022-04-26 15:30:02 +0800

    practice 811

--- homework/2 week/811/test.py
+++ homework/2 week/811/test.py
@@ -1,35 +1,19 @@
 class Solution:
-    def __init__(self):
-        self.arr = []
-        self.dic = {}
-        self.count = []
-
     def subdomainVisits(self, cpdomains):
-        sum_array = [0]
+        hashMap = {}
         ans = []
         for i in cpdomains:
-            a = i.split(" ")
-            self.merge(a[1], int(a[0]))
-        for j in self.count:
-            sum_array.append(j + sum_array[-1])
-
-        for index, k in enumerate(sum_array[1:len(self.arr) + 1]):
-            ans.append(str(k) + " " + self.arr[index])
-        return ans
-
-    def merge(self, string, num):
-        b = string.split(".")
-        for index in range(0, len(b)):
-            s = ".".join(b[index:])
-            if s not in self.dic:
-                self.dic[s] = len(self.arr)
-                self.arr.append(s)
-                if len(self.arr) > len(self.count):
-                    self.count.append(num)
+            box = i.split(" ")
+            count = int(box[0])
+            dns = box[1].split(".")
+            s = ""
+            for j in range(len(dns)-1, -1, -1):
+                s = dns[j] + "." + s
+                c = s[:-1]
+                if c not in hashMap:
+                    hashMap[c] = count
                 else:
-                    self.count[self.dic[s]] += num
-                self.count.append(-1 * num)
-            else:
-                self.count[self.dic[s]] += num
-                self.count[self.dic[s] + 1] -= num
-
+                    hashMap[c] += count
+        for i in hashMap.keys():
+            ans.append(str(hashMap[i]) + " " + i)
+        return ans
