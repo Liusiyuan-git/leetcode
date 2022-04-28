7082177f31902823a40b1228a0f8254993a20f74
liusiyuan <945212191@qq.com>
2022-04-28 15:43:06 +0800

    practice 130

--- homework/4 week/130/test.py
+++ homework/4 week/130/test.py
@@ -1,42 +1,30 @@
 class Solution:
-    def __init__(self):
-        self.n = 0  # 全局定义宽度
-        self.m = 0  # 全局定义高度
-        self.visited = {}  # 记录哪些点被访问过了
-        self.board = None  # 定义全局board
-        self.p = True  # 记录区域是否有边界点
-        self.ans = []  # 记录可以被置成"X"的坐标
-
     def solve(self, board) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        self.n = len(board)  # 宽度初始化
-        self.m = len(board[0])  # 高度初始化
-        self.board = board  # 全局board
+        self.n = len(board)
+        self.m = len(board[0])
+        for i in range(self.n):
+            self.dfs(board, i, 0)
+            self.dfs(board, i, self.m - 1)
+
+        for j in range(self.m):
+            self.dfs(board, 0, j)
+            self.dfs(board, self.n - 1, j)
+
         for i in range(self.n):
             for j in range(self.m):
-                if board[i][j] == "O" and self.num(i, j) not in self.visited:  # 如果是"O", 并且没有被访问过，则可以进行访问
-                    self.p = True  # 每次都要重置
-                    self.dfs(i, j)  # 开始 dfs探索
-                    if self.p:  # 区域合法，没有边界点，则可以置成 "X"
-                        for ans in self.ans:
-                            board[ans[0]][ans[1]] = "X"
-                    self.ans = []  # 重置 ans
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                if board[i][j] == "A":
+                    board[i][j] = "O"
 
-    def dfs(self, x, y):
-        self.visited[self.num(x, y)] = 1  # 进来了就表明该点访问过了
-        nx = [-1, 0, 0, 1]  # 方向数组，上下移动
-        ny = [0, -1, 1, 0]  # 方向数组，左右移动
-        for i in range(4):  # 开始探索四周的点
-            xx = x + nx[i]
-            yy = y + ny[i]
-            if xx < 0 or xx == self.n or yy < 0 or yy == self.m:  # 边界点，非法
-                self.p = False
-                continue
-            if self.board[xx][yy] == "O" and self.num(xx, yy) not in self.visited:  # 合法点
-                self.dfs(xx, yy)
-        self.ans.append([x, y])  # 将访问过的点，放置于 ans, 便于后续处理
-
-    def num(self, x, y):
-        return x * self.m + y
+    def dfs(self, board, x, y):
+        if x < 0 or x == self.n or y < 0 or y == self.m or board[x][y] != "O":
+            return
+        board[x][y] = "A"
+        self.dfs(board, x + 1, y)
+        self.dfs(board, x - 1, y)
+        self.dfs(board, x, y + 1)
+        self.dfs(board, x, y - 1)
