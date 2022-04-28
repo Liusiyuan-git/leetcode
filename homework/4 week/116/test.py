ac38c50e2281b4c52a6a5a18cf54f869c09c2116
liusiyuan <945212191@qq.com>
2022-04-28 19:55:21 +0800

    practice 116

---
+++ homework/4 week/116/test.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return root
+        queue = [root]
+        while queue:
+            n = []
+            for i in queue:
+                if i.left:
+                    n.append(i.left)
+                if i.right:
+                    n.append(i.right)
+            for i in range(len(n)):
+                if i > 0:
+                    n[i-1].next = n[i]
+            queue = n
+        return root
