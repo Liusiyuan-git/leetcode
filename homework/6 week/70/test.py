28b4aa6fe4de0ca3893b77dea07930cf30060837
liusiyuan <945212191@qq.com>
2022-04-30 17:30:50 +0800

    practice 70

--- homework/6 week/70/test.py
+++ homework/6 week/70/test.py
@@ -1,8 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n < 2:  # 确定边界
-            return 1
-        arr = [1, 1]
-        for i in range(2, n + 1):
-            arr.append(arr[i - 1] + arr[i - 2])  # 最优子结构，n为结束的楼梯，要么从 n-2 走两步上来， 要么从 n-1 走一步上来
-        return arr[-1]
+        f = [0] * (n+1)
+        f[0] = 1
+        f[1] = 1
+        for i in range(2, n+1):
+            f[i] = f[i - 1] + f[i - 2]
+        return f[n]
